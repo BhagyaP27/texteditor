927063b91903cd7da41b7ef944c4b565d1f3259e
Bhagya Patel <bhagyapatel2077@gmail.com>
2025-12-20 23:03:15 -0500

    feat: added the abilioty to create a new file and write to it

--- texteditor.cpp
+++ texteditor.cpp
@@ -1,139 +1,148 @@
 #include <iostream>
-#include <fstream> 
+#include <fstream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
+class TextEditor {
+private:
+    vector<string> lines;  // Store each line of text
+    string filename;       // Current file name
+    bool modified;         // Track if file has been modified
 
-class TextEditor{
-    private:
-        vector<string> lines; // stores each line of text
-        string filename; // name of the file being edited
-        bool modified; // track if the file has been modified
+public:
+    TextEditor() : modified(false) {}
 
-    public:
-        TextEditor() : modified(false) {}
-
-        //load the file into the editor
-        bool loadFile(const string& fname){
-            ifstream file(fname);
-            if(!file.is_open()){
-                cout <<" Could not open file: "<< fname << endl;
-                return false;
-            }
-            filename = fname;
-            lines.clear();
-            string line;
-
-            //read each line from the file
-            while(getline(file, line)){
-                lines.push_back(line);
-            }
-
-            file.close();
-            modified = false;
-            cout << "Loaded " << lines.size() << " lines from " << filename << endl;
-            return true;
+    // Load a file into the editor
+    bool loadFile(const string& fname) {
+        ifstream file(fname);
+        if (!file.is_open()) {
+            cout << "Could not open file: " << fname << endl;
+            return false;
         }
 
-        //Save current content to file
-        bool saveFile(){
-            if (filename.empty()){
-                cout << "no filename specifed." << endl;
-                return false;
-            }
+        filename = fname;
+        lines.clear();
+        string line;
+        
+        // Read file line by line
+        while (getline(file, line)) {
+            lines.push_back(line);
+        }
+        
+        file.close();
+        modified = false;
+        cout << "Loaded " << lines.size() << " lines from " << filename << endl;
+        return true;
+    }
 
-            ofstream file (filename);
-            if(!file.is_open()){
-                cout << "Could not open file for writing: " << filename << endl;
-                return false;
-            }
-
-            // Write each line to the file
-            for (const auto& line : lines){
-                file << line << endl;
-            }
-
-            file.close();
-            modified = false;
-            cout << "File saved successfully" << endl;
-            return true;
+    // Save the current content to file
+    bool saveFile() {
+        if (filename.empty()) {
+            cout << "No filename specified!" << endl;
+            return false;
         }
 
-        //Display current content
-        void display() {
-            cout << "\n--- " << (filename.empty() ? "New File" : filename) 
+        ofstream file(filename);
+        if (!file.is_open()) {
+            cout << "Could not save file: " << filename << endl;
+            return false;
+        }
+
+        // Write each line to file
+        for (const auto& line : lines) {
+            file << line << endl;
+        }
+
+        file.close();
+        modified = false;
+        cout << "File saved successfully!" << endl;
+        return true;
+    }
+
+    // Save with a specific filename (Save As)
+    bool saveAs(const string& fname) {
+        filename = fname;
+        return saveFile();
+    }
+
+    // Create a new empty file
+    void newFile() {
+        lines.clear();
+        filename = "";
+        modified = false;
+        cout << "New file created. Use 's <filename>' to save." << endl;
+    }
+
+    // Display all lines with line numbers
+    void display() {
+        cout << "\n--- " << (filename.empty() ? "New File" : filename) 
              << (modified ? " [Modified]" : "") << " ---\n";
-
-            for (size_t i = 0; i < lines.size(); i++) {
+        
+        for (size_t i = 0; i < lines.size(); i++) {
             cout << i + 1 << ": " << lines[i] << endl;
-            }
-            cout << "--- End of file ---\n\n";
         }
+        cout << "--- End of file ---\n\n";
+    }
 
-        // insert a new line at specdified position
-        void insertLine(size_t lineNum, const string& text){
-            if (lineNum > lines.size()){
-                lines.push_back(text);
-            } else {
-                lines.insert(lines.begin() + lineNum, text);
-            }
+    // Insert a new line at specified position
+    void insertLine(size_t lineNum, const string& text) {
+        if (lineNum > lines.size()) {
+            lines.push_back(text);
+        } else {
+            lines.insert(lines.begin() + lineNum, text);
+        }
+        modified = true;
+    }
 
+    // Delete a line at specified position
+    void deleteLine(size_t lineNum) {
+        if (lineNum < lines.size()) {
+            lines.erase(lines.begin() + lineNum);
             modified = true;
+            cout << "Line " << lineNum + 1 << " deleted." << endl;
+        } else {
+            cout << "Invalid line number!" << endl;
         }
+    }
 
-
-        // delete a line at a specified position
-        void deleteLine(size_t lineNum){
-            if (lineNum >= lines.size()){
-                lines.erase(lines.begin() + lineNum);
-                modified = true;
-                cout << "line" << lineNum +1 << " deleted." << endl;
-
-            } else {
-                cout << "Invalid line number." << endl;
-            }
+    // Edit an existing line
+    void editLine(size_t lineNum, const string& newText) {
+        if (lineNum < lines.size()) {
+            lines[lineNum] = newText;
+            modified = true;
+            cout << "Line " << lineNum + 1 << " updated." << endl;
+        } else {
+            cout << "Invalid line number!" << endl;
         }
+    }
 
-        //edit an existing line
-        void editLine(size_t lineNum, const string& newText){
-            if (lineNum < lines.size()){
-                lines[lineNum] = newText;
-                modified = true;
-            } else {
-                cout << "Invalid line number." << endl;
-            }
-        }
+    // Check if file has been modified
+    bool isModified() const {
+        return modified;
+    }
 
-        // check if the file has been modified
-        bool isModified() const {
-            return modified;
-        }
-
-        // get number of lines
-        size_t getLineCount() const {
-            return lines.size();
-        }
+    // Get number of lines
+    size_t getLineCount() const {
+        return lines.size();
+    }
 };
 
-// printing a list of commands
-
-void printHelp(){
-    cout << "\nText Editor Commands:\n";
-    cout << "  l <filename> : Load file\n";
-    cout << "  s             : Save file\n";
-    cout << "  d             : Display content\n";
-    cout << "  i <line> <text> : Insert line at position\n";
-    cout << "  e <line> <text> : Edit line at position\n";
-    cout << "  r <line>      : Delete line at position\n";
-    cout << "  h             : Help\n";
-    cout << "  q             : Quit\n";
+void printHelp() {
+    cout << "\nCommands:\n";
+    cout << "  n             - Create new file\n";
+    cout << "  l <filename>  - Load file\n";
+    cout << "  s [filename]  - Save file (optionally with new name)\n";
+    cout << "  d             - Display file\n";
+    cout << "  i <line>      - Insert text at line number (0 for beginning)\n";
+    cout << "  e <line>      - Edit line number\n";
+    cout << "  x <line>      - Delete line number\n";
+    cout << "  h             - Show this help\n";
+    cout << "  q             - Quit\n";
     cout << endl;
 }
 
-
-// main loop
 int main() {
     TextEditor editor;
     string command;
@@ -157,13 +166,31 @@ int main() {
             cout << "Goodbye!" << endl;
             break;
         }
+        else if (command == "n") {
+            if (editor.isModified()) {
+                cout << "Current file has unsaved changes. Continue? (y/n): ";
+                char response;
+                cin >> response;
+                if (response != 'y' && response != 'Y') {
+                    continue;
+                }
+            }
+            editor.newFile();
+        }
         else if (command == "l") {
             string fname;
             cin >> fname;
             editor.loadFile(fname);
         }
         else if (command == "s") {
-            editor.saveFile();
+            string fname;
+            // Check if there's a filename after 's'
+            if (cin.peek() != '\n') {
+                cin >> fname;
+                editor.saveAs(fname);
+            } else {
+                editor.saveFile();
+            }
         }
         else if (command == "d") {
             editor.display();
